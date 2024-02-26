a77a10d310fd1b72907a8164ac6834bd043a7e05
asheroto <github@asheroto.com>
2024-02-26 14:22:39 -0600

    Add UpdateSelf param, fix CheckForUpdates, update readme

--- .github/README.md
+++ .github/README.md
@@ -1,7 +1,12 @@
+[![PowerShell Gallery Downloads](https://img.shields.io/powershellgallery/dt/BlockFolderWindowsFirewall?label=PowerShell%20Gallery%20downloads)](https://www.powershellgallery.com/packages/BlockFolderWindowsFirewall)
+[![GitHub Downloads - All Releases](https://img.shields.io/github/downloads/asheroto/BlockFolderWindowsFirewall/total?label=release%20downloads)](https://github.com/asheroto/BlockFolderWindowsFirewall/releases)
+[![Release](https://img.shields.io/github/v/release/asheroto/BlockFolderWindowsFirewall)](https://github.com/asheroto/BlockFolderWindowsFirewall/releases)
 [![GitHub Release Date - Published_At](https://img.shields.io/github/release-date/asheroto/BlockFolderWindowsFirewall)](https://github.com/asheroto/BlockFolderWindowsFirewall/releases)
-[![GitHub Downloads - All Releases](https://img.shields.io/github/downloads/asheroto/BlockFolderWindowsFirewall/total)](https://github.com/asheroto/BlockFolderWindowsFirewall/releases)
-[![GitHub Sponsor](https://img.shields.io/github/sponsors/asheroto?label=Sponsor&logo=GitHub)](https://github.com/sponsors/asheroto)
-[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/asheroto)
+
+[![GitHub Sponsor](https://img.shields.io/github/sponsors/asheroto?label=Sponsor&logo=GitHub)](https://github.com/sponsors/asheroto?frequency=one-time&sponsor=asheroto)
+<a href="https://ko-fi.com/asheroto"><img src="https://ko-fi.com/img/githubbutton_sm.svg" alt="Ko-Fi Button" height="20px"></a>
+<a href="https://www.buymeacoffee.com/asheroto"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=seb6596&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff](https://img.buymeacoffee.com/button-api/?text=Buy%20me%20a%20coffee&emoji=&slug=asheroto&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff)" height="40px"></a>
+
 # Block Entire Folder in Windows Firewall
 
 This script provides functionalities to manage (block/unblock) executables (EXEs) in specified directories via Windows Firewall. The operation can be targeted at individual directories or recursively applied to subdirectories as well.
@@ -30,32 +35,34 @@ BlockFolderWindowsFirewall [-Path <String>] [-Outbound] [-Inbound] [-Recurse] [-
 
 # Parameters
 
-|Parameter|Required|Description|
-|--|--|--|
-|`-Path`|Yes|The folder of EXEs to block/unblock.|
-|`-Recurse`|No|Scans the folder and subfolders for EXEs.|
-|`-UnblockInstead`|No|Unblocks the EXEs in a folder or subfolder.|
-|`-Inbound`|No|Blocks all inbound connections for the EXEs in a folder or subfolder.|
-|`-Outbound`|No|Blocks all outbound connections for the EXEs in a folder or subfolder.|
-|`-Help`|No|Displays the help message.|
-|`-Version`|No|Shows the current version of the script.|
-|`-CheckForUpdate`|No|Checks if there is an update available for the script.|
+| Parameter         | Required | Description                                                            |
+| ----------------- | -------- | ---------------------------------------------------------------------- |
+| `-Path`           | Yes      | The folder of EXEs to block/unblock.                                   |
+| `-Recurse`        | No       | Scans the folder and subfolders for EXEs.                              |
+| `-UnblockInstead` | No       | Unblocks the EXEs in a folder or subfolder.                            |
+| `-Inbound`        | No       | Blocks all inbound connections for the EXEs in a folder or subfolder.  |
+| `-Outbound`       | No       | Blocks all outbound connections for the EXEs in a folder or subfolder. |
+| `-CheckForUpdate` | No       | Checks if there is an update available for the script.                 |
+| `-UpdateSelf`     | No       | Updates the script to the latest version.                              |
+| `-Help`           | No       | Displays the help message.                                             |
+| `-Version`        | No       | Shows the current version of the script.                               |
 
 # Examples
 
-|Description|Command|
-|--|--|
-|Block all EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder"`|
-|Block all EXEs in a specified folder and its subfolders|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Recurse`|
-|Block only the inbound connections for EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Inbound`|
-|Block only the outbound connections for EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Outbound`|
-|Unblock all EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead`|
-|Unblock all EXEs in a specified folder and its subfolders|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Recurse -UnblockInstead`|
-|Unblock only the inbound connections for EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead -Inbound`|
-|Unblock only the outbound connections for EXEs in a specified folder|`BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead -Outbound`|
-|Display help for the script|`BlockFolderWindowsFirewall -Help`|
-|Check the current version of the script|`BlockFolderWindowsFirewall -Version`|
-|Check if there is an update available for the script|`BlockFolderWindowsFirewall -CheckForUpdate`|
+| Description                                                          | Command                                                                            |
+| -------------------------------------------------------------------- | ---------------------------------------------------------------------------------- |
+| Block all EXEs in a specified folder                                 | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder"`                           |
+| Block all EXEs in a specified folder and its subfolders              | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Recurse`                  |
+| Block only the inbound connections for EXEs in a specified folder    | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Inbound`                  |
+| Block only the outbound connections for EXEs in a specified folder   | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Outbound`                 |
+| Unblock all EXEs in a specified folder                               | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead`           |
+| Unblock all EXEs in a specified folder and its subfolders            | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Recurse -UnblockInstead`  |
+| Unblock only the inbound connections for EXEs in a specified folder  | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead -Inbound`  |
+| Unblock only the outbound connections for EXEs in a specified folder | `BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -UnblockInstead -Outbound` |
+| Display help for the script                                          | `BlockFolderWindowsFirewall -Help`                                                 |
+| Check the current version of the script                              | `BlockFolderWindowsFirewall -Version`                                              |
+| Check if there is an update available for the script                 | `BlockFolderWindowsFirewall -CheckForUpdate`                                       |
 
 # Screenshot
+
 ![Screenshot](https://github.com/asheroto/BlockFolderWindowsFirewall/assets/49938263/439d99ed-0ad1-4f75-87bc-11d385185567)
--- BlockFolderWindowsFirewall.ps1
+++ BlockFolderWindowsFirewall.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0.1
+.VERSION 1.0.2
 
 .GUID c7a7f36d-3d4f-4e2e-9bec-336ec8a0eb16
 
@@ -18,6 +18,7 @@
 [Version 0.0.3] - Updated code signing certificate.
 [Version 1.0.0] - Major improvements. Added -Version, -Help, -CheckForUpdate. Added inbound/outbound option (will do both if not specified).
 [Version 1.0.1] - Added additional line breaks, changed first line to warning, and added PowerShell gallery info to -CheckForUpdate
+[Version 1.0.2] - Added UpdateSelf function, fixed CheckForUpdate function.
 
 #>
 
@@ -62,8 +63,26 @@
 .EXAMPLE
 	BlockFolderWindowsFirewall -Path "C:\Folder\Subfolder" -Outbound -UnblockInstead
 	This command will unblock only outbound traffic for all EXEs in the specified folder.
+.PARAMETER Path
+    The directory path to target for the operation.
+.PARAMETER Recurse
+    Include all subdirectories in the operation.
+.PARAMETER UnblockInstead
+    Change the operation mode from blocking to unblocking.
+.PARAMETER Inbound
+    Only affect Inbound connections.
+.PARAMETER Outbound
+    Only affect Outbound connections.
+.PARAMETER CheckForUpdate
+    Verify if the current script version is up-to-date.
+.PARAMETER UpdateSelf
+    Updates the script to the latest version on PSGallery.
+.PARAMETER Version
+    Display the current version of the script.
+.PARAMETER Help
+    Bring up the help information for the script usage.
 .NOTES
-    Version      : 1.0.1
+    Version      : 1.0.2
     Created by   : asheroto
 .LINK
     Project Site: https://github.com/asheroto/BlockFolderWindowsFirewall
@@ -73,214 +92,288 @@
 
 [CmdletBinding()]
 param (
-	[String]$Path,
-	[Switch]$Outbound,
-	[Switch]$Inbound,
-	[Switch]$Recurse,
-	[Switch]$UnblockInstead,
-	[switch]$Version,
-	[switch]$Help,
-	[switch]$CheckForUpdate
+    [String]$Path,
+    [Switch]$Outbound,
+    [Switch]$Inbound,
+    [Switch]$Recurse,
+    [Switch]$UnblockInstead,
+    [switch]$CheckForUpdate,
+    [switch]$UpdateSelf,
+    [switch]$Version,
+    [switch]$Help
 )
 
-# If Path is not set, check if -Version, -Help, or -CheckForUpdate is specified
-if ("" -eq $Path) {
-	if ($Version.IsPresent -or $Help.IsPresent -or $CheckForUpdate.IsPresent) {
-		# Do nothing
-	} else {
-		Write-Error "-Path is required"
-		exit 1
-	}
-}
-
-# If -Recurse or -UnblockInstead is specified, -Path is required
-if ($Recurse.IsPresent -or $UnblockInstead.IsPresent -or $Outbound.IsPresent -or $Inbound.IsPresent) {
-	if ("" -eq $Path) {
-		Write-Error "-Path is required"
-		exit 1
-	}
-}
-
 # Version
-$CurrentVersion = '1.0.1'
+$CurrentVersion = '1.0.2'
 $RepoOwner = 'asheroto'
 $RepoName = 'BlockFolderWindowsFirewall'
+$PowerShellGalleryName = 'BlockFolderWindowsFirewall'
 
-# Check if -Version is specified
+# Display version if -Version is specified
 if ($Version.IsPresent) {
-	Write-Output "$RepoName $CurrentVersion"
-	exit 0
+    $CurrentVersion
+    exit 0
 }
 
 # Help
 if ($Help) {
-	Get-Help -Name $MyInvocation.MyCommand.Source -Full
-	exit 0
+    Get-Help -Name $MyInvocation.MyCommand.Source -Full
+    exit 0
 }
 
-function Check-GitHubRelease {
-	param (
-		[string]$Owner,
-		[string]$Repo
-	)
-	try {
-		$url = "https://api.github.com/repos/$Owner/$Repo/releases/latest"
-		$response = Invoke-RestMethod -Uri $url -ErrorAction Stop
+function Get-GitHubRelease {
+    <#
+        .SYNOPSIS
+        Fetches the latest release information of a GitHub repository.
 
-		$latestVersion = $response.tag_name
-		$publishedAt = $response.published_at
-		$UtcDateTimeFormat = "MM/dd/yyyy HH:mm:ss"
+        .DESCRIPTION
+        This function uses the GitHub API to get information about the latest release of a specified repository, including its version and the date it was published.
 
-		# Convert UTC time string to local time
-		$UtcDateTime = [DateTime]::ParseExact($publishedAt, $UtcDateTimeFormat, $null)
-		$PublishedLocalDateTime = $UtcDateTime.ToLocalTime()
+        .PARAMETER Owner
+        The GitHub username of the repository owner.
 
-		[PSCustomObject]@{
-			LatestVersion     = $latestVersion
-			PublishedDateTime = $PublishedLocalDateTime
-		}
-	} catch {
-		Write-Error "Unable to check for updates. Error: $_"
-		exit 1
-	}
+        .PARAMETER Repo
+        The name of the repository.
+
+        .EXAMPLE
+        Get-GitHubRelease -Owner "asheroto" -Repo "winget-install"
+        This command retrieves the latest release version and published datetime of the winget-install repository owned by asheroto.
+    #>
+    [CmdletBinding()]
+    param (
+        [string]$Owner,
+        [string]$Repo
+    )
+    try {
+        $url = "https://api.github.com/repos/$Owner/$Repo/releases/latest"
+        $response = Invoke-RestMethod -Uri $url -ErrorAction Stop
+
+        $latestVersion = $response.tag_name
+        $publishedAt = $response.published_at
+
+        # Convert UTC time string to local time
+        $UtcDateTime = [DateTime]::Parse($publishedAt, [System.Globalization.CultureInfo]::InvariantCulture, [System.Globalization.DateTimeStyles]::RoundtripKind)
+        $PublishedLocalDateTime = $UtcDateTime.ToLocalTime()
+
+        [PSCustomObject]@{
+            LatestVersion     = $latestVersion
+            PublishedDateTime = $PublishedLocalDateTime
+        }
+    } catch {
+        Write-Error "Unable to check for updates.`nError: $_"
+        exit 1
+    }
 }
 
-# Check for updates
-if ($CheckForUpdate) {
-	$Data = Check-GitHubRelease -Owner $RepoOwner -Repo $RepoName
+function CheckForUpdate {
+    param (
+        [string]$RepoOwner,
+        [string]$RepoName,
+        [version]$CurrentVersion,
+        [string]$PowerShellGalleryName
+    )
 
-	if ($Data.LatestVersion -gt $CurrentVersion) {
-		Write-Output ""
-		Write-Warning "$RepoName is out of date.`n"
-		Write-Output "Current version: $CurrentVersion. Latest version: $($Data.LatestVersion). Published at: $($Data.PublishedDateTime).`n"
-		Write-Output "You can download the latest version from https://github.com/$RepoOwner/$RepoName/releases`n"
-		Write-Output "If you have PowerShell 5 or later, you can run"
-		Write-Output "`tInstall-Script -Name $RepoName -Force"
-		Write-Output "to install the latest version.`n"
-	} else {
-		Write-Output "`n$RepoName is up to date.`n"
-		Write-Output "Current version: $CurrentVersion. Latest version: $($Data.LatestVersion). Published at: $($Data.PublishedDateTime).`n"
-		Write-Output "Repository: https://github.com/$RepoOwner/$RepoName/releases`n"
-		Write-Output "PowerShell Gallery: https://www.powershellgallery.com/packages/$RepoName`n"
-	}
-	exit 0
+    $Data = Get-GitHubRelease -Owner $RepoOwner -Repo $RepoName
+
+    Write-Output ""
+    Write-Output ("Repository:       {0,-40}" -f "https://github.com/$RepoOwner/$RepoName")
+    Write-Output ("Current Version:  {0,-40}" -f $CurrentVersion)
+    Write-Output ("Latest Version:   {0,-40}" -f $Data.LatestVersion)
+    Write-Output ("Published at:     {0,-40}" -f $Data.PublishedDateTime)
+
+    if ($Data.LatestVersion -gt $CurrentVersion) {
+        Write-Output ("Status:           {0,-40}" -f "A new version is available.")
+        Write-Output "`nOptions to update:"
+        Write-Output "- Download latest release: https://github.com/$RepoOwner/$RepoName/releases"
+        if ($PowerShellGalleryName) {
+            Write-Output "- Run: $RepoName -UpdateSelf"
+            Write-Output "- Run: Install-Script $PowerShellGalleryName -Force"
+        }
+    } else {
+        Write-Output ("Status:           {0,-40}" -f "Up to date.")
+    }
+    exit 0
+}
+
+function UpdateSelf {
+    try {
+        # Get PSGallery version of script
+        $psGalleryScriptVersion = (Find-Script -Name $PowerShellGalleryName).Version
+
+        # If the current version is less than the PSGallery version, update the script
+        if ($CurrentVersion -lt $psGalleryScriptVersion) {
+            Write-Output "Updating script to version $psGalleryScriptVersion..."
+
+            # Install NuGet PackageProvider if not already installed
+            if (-not (Get-PackageProvider -Name NuGet -ErrorAction SilentlyContinue)) {
+                Install-PackageProvider -Name "NuGet" -Force
+            }
+
+            # Trust the PSGallery if not already trusted
+            $psRepoInstallationPolicy = (Get-PSRepository -Name 'PSGallery').InstallationPolicy
+            if ($psRepoInstallationPolicy -ne 'Trusted') {
+                Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted | Out-Null
+            }
+
+            # Update the script
+            Install-Script $PowerShellGalleryName -Force
+
+            # If PSGallery was not trusted, reset it to its original state
+            if ($psRepoInstallationPolicy -ne 'Trusted') {
+                Set-PSRepository -Name 'PSGallery' -InstallationPolicy $psRepoInstallationPolicy | Out-Null
+            }
+
+            Write-Output "Script updated to version $psGalleryScriptVersion."
+            exit 0
+        } else {
+            Write-Output "Script is already up to date."
+            exit 0
+        }
+    } catch {
+        Write-Output "An error occurred: $_"
+        exit 1
+    }
 }
 
 function Block-Exe {
-	param(
-		[String]$FileName,
-		[String]$FilePath,
-		[Switch]$Inbound,
-		[Switch]$Outbound
-	)
+    param(
+        [String]$FileName,
+        [String]$FilePath,
+        [Switch]$Inbound,
+        [Switch]$Outbound
+    )
 
-	If ($Inbound.IsPresent -eq $false -and $Outbound.IsPresent -eq $false) {
-		Write-Output "Blocking Inbound and Outbound $FilePath..."
-		New-NetFirewallRule -DisplayName "$FileName" -Direction Inbound -Program $FilePath -Action Block | Out-Null
-		New-NetFirewallRule -DisplayName "$FileName" -Direction Outbound -Program $FilePath -Action Block | Out-Null
-	} elseif ($Inbound.IsPresent) {
-		Write-Output "Blocking Inbound $FilePath..."
-		New-NetFirewallRule -DisplayName "$FileName" -Direction Inbound -Program $FilePath -Action Block | Out-Null
-	} elseif ($Outbound.IsPresent) {
-		Write-Output "Blocking Outbound $FilePath..."
-		New-NetFirewallRule -DisplayName "$FileName" -Direction Outbound -Program $FilePath -Action Block | Out-Null
-	}
+    If ($Inbound.IsPresent -eq $false -and $Outbound.IsPresent -eq $false) {
+        Write-Output "Blocking Inbound and Outbound $FilePath..."
+        New-NetFirewallRule -DisplayName "$FileName" -Direction Inbound -Program $FilePath -Action Block | Out-Null
+        New-NetFirewallRule -DisplayName "$FileName" -Direction Outbound -Program $FilePath -Action Block | Out-Null
+    } elseif ($Inbound.IsPresent) {
+        Write-Output "Blocking Inbound $FilePath..."
+        New-NetFirewallRule -DisplayName "$FileName" -Direction Inbound -Program $FilePath -Action Block | Out-Null
+    } elseif ($Outbound.IsPresent) {
+        Write-Output "Blocking Outbound $FilePath..."
+        New-NetFirewallRule -DisplayName "$FileName" -Direction Outbound -Program $FilePath -Action Block | Out-Null
+    }
 }
 
 function Unblock-Exe {
-	param(
-		[String]$FilePath,
-		[Switch]$Inbound,
-		[Switch]$Outbound
-	)
+    param(
+        [String]$FilePath,
+        [Switch]$Inbound,
+        [Switch]$Outbound
+    )
 
-	if ($Inbound.IsPresent -eq $false -and $Outbound.IsPresent -eq $false) {
-		Write-Output "Unblocking Inbound and Outbound $FilePath..."
-		$matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID }
-		if ($matchingRules) {
-			$matchingRules | Remove-NetFirewallRule
-		} else {
-			Write-Warning "No matching Inbound and Outbound rules found for $FilePath"
-		}
-	} elseif ($Inbound.IsPresent) {
-		Write-Output "Unblocking Inbound $FilePath..."
-		$matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID | ? { $_.Direction -eq "Inbound" } }
-		if ($matchingRules) {
-			$matchingRules | Remove-NetFirewallRule
-		} else {
-			Write-Warning "No matching Inbound rules found for $FilePath"
-		}
-	} elseif ($Outbound.IsPresent) {
-		Write-Output "Unblocking Outbound $FilePath..."
-		$matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID | ? { $_.Direction -eq "Outbound" } }
-		if ($matchingRules) {
-			$matchingRules | Remove-NetFirewallRule
-		} else {
-			Write-Warning "No matching Outbound rules found for $FilePath"
-		}
-	}
+    if ($Inbound.IsPresent -eq $false -and $Outbound.IsPresent -eq $false) {
+        Write-Output "Unblocking Inbound and Outbound $FilePath..."
+        $matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID }
+        if ($matchingRules) {
+            $matchingRules | Remove-NetFirewallRule
+        } else {
+            Write-Warning "No matching Inbound and Outbound rules found for $FilePath"
+        }
+    } elseif ($Inbound.IsPresent) {
+        Write-Output "Unblocking Inbound $FilePath..."
+        $matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID | ? { $_.Direction -eq "Inbound" } }
+        if ($matchingRules) {
+            $matchingRules | Remove-NetFirewallRule
+        } else {
+            Write-Warning "No matching Inbound rules found for $FilePath"
+        }
+    } elseif ($Outbound.IsPresent) {
+        Write-Output "Unblocking Outbound $FilePath..."
+        $matchingRules = Get-NetFirewallApplicationFilter | ? { $_.AppPath -eq "$FilePath" } | % { Get-NetFirewallRule $_.InstanceID | ? { $_.Direction -eq "Outbound" } }
+        if ($matchingRules) {
+            $matchingRules | Remove-NetFirewallRule
+        } else {
+            Write-Warning "No matching Outbound rules found for $FilePath"
+        }
+    }
+}
+
+# Update the script if -UpdateSelf is specified
+if ($UpdateSelf) { UpdateSelf }
+
+# Check for updates if -CheckForUpdate is specified
+if ($CheckForUpdate) { CheckForUpdate -RepoOwner $RepoOwner -RepoName $RepoName -CurrentVersion $CurrentVersion -PowerShellGalleryName $PowerShellGalleryName }
+
+# If Path is not set, check if -Version, -Help, or -CheckForUpdate is specified
+if ("" -eq $Path) {
+    if ($Version.IsPresent -or $Help.IsPresent -or $CheckForUpdate.IsPresent) {
+        # Do nothing
+    } else {
+        Write-Error "-Path is required"
+        exit 1
+    }
+}
+
+# If -Recurse or -UnblockInstead is specified, -Path is required
+if ($Recurse.IsPresent -or $UnblockInstead.IsPresent -or $Outbound.IsPresent -or $Inbound.IsPresent) {
+    if ("" -eq $Path) {
+        Write-Error "-Path is required"
+        exit 1
+    }
 }
 
 # Confirm folder path exists
-If (Test-Path -Path $Path -PathType Container) {
-	# Update note
-	Write-Output "`n$RepoName $CurrentVersion"
-	Write-Output "To check for updates, run $RepoName -CheckForUpdate`n"
+if (Test-Path -Path $Path -PathType Container) {
+    # Update note
+    Write-Output "`n$RepoName $CurrentVersion"
+    Write-Output "To check for updates, run $RepoName -CheckForUpdate`n"
 
-	# Folder path exists
-	Write-Output "Folder exists, continuing...`n"
+    # Folder path exists
+    Write-Output "Folder exists, continuing...`n"
 
-	# Pause for 10 seconds if the folder is C:\
-	If ($Path -eq "C:\") {
-		Write-Output "WARNING: You specified C:\ as the path, this will block all EXEs on the C drive. Press CTRL+C to cancel, or wait 10 seconds to continue..."
-		Start-Sleep -Seconds 10
-	}
+    # Pause for 10 seconds if the folder is C:\
+    If ($Path -eq "C:\") {
+        Write-Output "WARNING: You specified C:\ as the path, this will block all EXEs on the C drive. Press CTRL+C to cancel, or wait 10 seconds to continue..."
+        Start-Sleep -Seconds 10
+    }
 
-	# Determine if we're blocking or unblocking
-	if ($UnblockInstead) {
-		# Unblocking
-		if ($Recurse) {
-			# Recursively unblocking
-			Write-Output "Recursively unblocking EXEs in $Path..."
-			Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | ForEach-Object {
-				$FilePath = $_.FullName
-				$FileName = Split-Path $FilePath -Leaf
-				Unblock-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
-			}
-		} else {
-			# Not recursively unblocking
-			Write-Output "Unblocking EXEs in $Path..."
-			Get-ChildItem -Path $Path -Filter "*.exe" | ForEach-Object {
-				$FilePath = $_.FullName
-				$FileName = Split-Path $FilePath -Leaf
-				Unblock-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
-			}
-		}
-	} else {
-		# Blocking
-		if ($Recurse) {
-			# Recursively blocking
-			Write-Output "Recursively blocking EXEs in $Path..."
-			Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | ForEach-Object {
-				$FilePath = $_.FullName
-				$FileName = Split-Path $FilePath -Leaf
-				Block-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
-			}
-		} else {
-			# Not recursively blocking
-			Write-Output "Blocking EXEs in $Path..."
-			Get-ChildItem -Path $Path -Filter "*.exe" | ForEach-Object {
-				$FilePath = $_.FullName
-				$FileName = Split-Path $FilePath -Leaf
-				Block-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
-			}
-		}
-	}
+    # Determine if we're blocking or unblocking
+    if ($UnblockInstead) {
+        # Unblocking
+        if ($Recurse) {
+            # Recursively unblocking
+            Write-Output "Recursively unblocking EXEs in $Path..."
+            Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | ForEach-Object {
+                $FilePath = $_.FullName
+                $FileName = Split-Path $FilePath -Leaf
+                Unblock-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
+            }
+        } else {
+            # Not recursively unblocking
+            Write-Output "Unblocking EXEs in $Path..."
+            Get-ChildItem -Path $Path -Filter "*.exe" | ForEach-Object {
+                $FilePath = $_.FullName
+                $FileName = Split-Path $FilePath -Leaf
+                Unblock-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
+            }
+        }
+    } else {
+        # Blocking
+        if ($Recurse) {
+            # Recursively blocking
+            Write-Output "Recursively blocking EXEs in $Path..."
+            Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | ForEach-Object {
+                $FilePath = $_.FullName
+                $FileName = Split-Path $FilePath -Leaf
+                Block-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
+            }
+        } else {
+            # Not recursively blocking
+            Write-Output "Blocking EXEs in $Path..."
+            Get-ChildItem -Path $Path -Filter "*.exe" | ForEach-Object {
+                $FilePath = $_.FullName
+                $FileName = Split-Path $FilePath -Leaf
+                Block-Exe -FilePath $FilePath -FileName $FileName -Inbound:$Inbound.IsPresent -Outbound:$Outbound.IsPresent
+            }
+        }
+    }
 
-	Write-Output "Done!"
+    Write-Output "Done!"
 } else {
-	# Folder path does not exist
-	Write-Output "Folder does NOT exist, please run script again with a valid path"
+    # Folder path does not exist
+    Write-Output "Folder does NOT exist, please run script again with a valid path"
 }
 
 Write-Output ""
